f3879af971bf820d3acb57a6cd707e54592cfc58
Issaret Prachitmutita <imac.monochrome@gmail.com>
2017-08-25 21:34:29 +0700

    first commit

---
+++ src/components/ButtonCounter.vue
@@ -0,0 +1,27 @@
+<template>
+  <button v-on:click="increment">
+    {{ counter }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'buttonCounter',
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increment: function() {
+      this.counter++
+      this.$emit('increment')
+    }
+  },
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="counter-event-example">
+    <p>{{ total }}</p>
+    <buttonCounter @increment="incrementTotal"></buttonCounter>
+    <buttonCounter @increment="incrementTotal"></buttonCounter>
+  </div>
+</template>
+
+<script>
+import buttonCounter from '@/components/ButtonCounter'
+
+
+export default {
+  name: 'hello',
+  components: {
+    buttonCounter
+  },
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      total: 0
+    }
+  },
+  methods: {
+    incrementTotal: function() {
+      this.total++
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
